e10e834e8114b1e27f1abbdeef452b0d9d569191
Manan <manan.patel@kevit.io>
2026-09-16 18:25:33 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Console.WriteLine("Hello, World!");
+//float f1 = 35e3F;
+//double d1 = 12E4D;
+//Console.WriteLine(f1);
+//Console.WriteLine(d1);
+
+//bool isCSharpFun = true;
+//bool isFishTasty = false;
+//Console.WriteLine(isCSharpFun);   // Outputs True
+//Console.WriteLine(isFishTasty);
+
+//double db = 129000; 
+//int myint = (int)db;
+//Console.WriteLine(myint);
+
+//int myInt = 10;
+//double myDouble = 5.25;
+//bool myBool = true;
+
+//Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+//Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+//Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+//Console.WriteLine(Convert.ToString(myBool));
